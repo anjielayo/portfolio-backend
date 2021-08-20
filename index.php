35d6fed824db077ac1675bc7312d14bd02513205
Anjolaoluwa Olutomilayo <anjolaolutomilayo@gmail.com>
2021-08-20 07:26:42 +0100

    Modified php

--- index.php
+++ index.php
@@ -9,27 +9,6 @@
     $headers = "From: $email_addressn";
     $sent = mail('you@domain.com', 'Feedback Form Submission', $feedback, $headers);
     if ($sent) {
-        // database connection code
-        // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
-
-        $con = mysqli_connect('localhost', 'root', '', 'db_contact');
-
-        // get the post records
-        $txtName = $_POST['txtName'];
-        $txtEmail = $_POST['txtEmail'];
-        $txtSubject = $_POST['txtSubject'];
-        $txtMessage = $_POST['txtMessage'];
-
-        // database insert SQL code
-        $sql = "INSERT INTO `tbl_contact` (`Id`, `fldName`, `fldEmail`, `fldSubject`, `fldMessage`) VALUES ('0', '$txtName', '$txtEmail', '$txtSubject', '$txtMessage')";
-
-        // insert in database 
-        $rs = mysqli_query($con, $sql);
-
-        if ($rs) {
-            echo "Contact Records Inserted";
-        }
-
     ?><html>
 
      <head>
